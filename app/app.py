69cafe55a2a9ea9fd1e57c438de7a1ce0fee09c8
Adam Deacon <adeacon@hotmail.com>
2021-02-20 22:01:19 +0000

    Update app.py
    Auto-formatting

--- app/app.py
+++ app/app.py
@@ -1,9 +1,9 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.graph_objects as go
 import numpy as np
 import pandas as pd
+import plotly.graph_objects as go
 
 import src.utilities as utilities
 
@@ -11,27 +11,31 @@ data = utilities.get_master("quality")
 overall_score = data.score.mean()
 # range_max = data.score.max()
 
-data_pivot = pd.pivot_table(data, values="score", index="file", columns="category", aggfunc=np.mean, margins=False)
+data_pivot = pd.pivot_table(
+    data,
+    values="score",
+    index="file",
+    columns="category",
+    aggfunc=np.mean,
+    margins=False,
+)
 # print(data_pivot)
 
-fig = go.Figure(data=go.Heatmap(
+fig = go.Figure(
+    data=go.Heatmap(
         z=data_pivot.values,
         x=data_pivot.columns,
         y=data_pivot.index,
-        colorscale='Viridis',
-        colorbar=dict(title="<b>Average Score</b>")
-        )
-        )
+        colorscale="Viridis",
+        colorbar=dict(title="<b>Average Score</b>"),
+    )
+)
 
 fig.update_layout(
     # title='Average Score by Category and File',
-    xaxis=dict(
-        title="<b>Category</b>"
-    ),
-    yaxis=dict(
-        title="<b>File</b>", autorange='reversed'
-    ),
-    )
+    xaxis=dict(title="<b>Category</b>"),
+    yaxis=dict(title="<b>File</b>", autorange="reversed"),
+)
 
 fig.update_xaxes(side="top")
 
@@ -44,9 +48,7 @@ app.layout = html.Div(
     children=[
         html.Div(
             children=[
-                html.H1(
-                    children="Data Quality Dashboard", className="header-title"
-                ),
+                html.H1(children="Data Quality Dashboard", className="header-title"),
                 html.P(
                     children="Various data quality metrics"
                     " aggregated by category and file."
